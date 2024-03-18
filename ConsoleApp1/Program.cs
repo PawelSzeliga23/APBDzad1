51e61f7e3bb6303e9f9adeb56d5ecc1fb6ab94fa
s27030 <s27030@pjwstk.edu.pl>
2024-03-18 09:20:04 +0100

    Modyfikacja 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,5 +8,6 @@ public class Program
         Console.WriteLine("Hello World!");
         Console.WriteLine("Modification 1");
         Console.WriteLine("Modification 2");
+        Console.WriteLine("Modification 3");
     }
 }
